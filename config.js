c1e82335558645c670ba4183e4253b2bd4756e44
RaulJimenezOrtega <hhkaos@gmail.com>
2019-09-29 00:54:59 +0200

    Changing categories and spreadsheet

--- config.js
+++ config.js
@@ -1,18 +1,16 @@
 const spreadsheetsId = '1e4GZKOYSND3BRFiW8DJP846s4ecuu8A0wg9PXcVhJ70';
 const spreadsheetsTab = 1;
 const modalFields = [
-        'title', 'description', 'level', 'length', 'speakers', 'comments'
+        'title', 'description', 'length', 'comments'
     ];
 
 const tags = [
-    ['Herramientas y lenguajes de programación', 'programming'],
-    ['Tendencias tecnológicas', 'future'],
-    ['Robótica, electrónica', 'robotics'],
-    ['Herramientas y técnicas de productividad', 'post-it'],
-    ['Marketing online', 'marketing'],
-    ['Diseño gráfico', 'design'],
-    ['Emprendimiento', 'business'],
-    ['Buen uso de la tecnología', 'cyber-security']
+    ['Gestión emocional', 'kids'],
+    ['Deportes y salud', 'sports'],
+    ['Idiomas', 'language'],
+    ['Habilidades y aptitudes', 'skills'],
+    ['Valores o actitudes', 'values'],
+    ['Nuevas tecnologías ', 'technology']
 ];
 
 const colorPalette = ['#DB4367', '#EB4B38', '#AC4988', '#705390', '#3F5279', '#2F4857'];
