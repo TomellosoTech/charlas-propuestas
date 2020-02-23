68526fa07b5c9088fb7254026abe1e9459798e6e
RaulJimenezOrtega <hhkaos@gmail.com>
2020-02-24 00:36:07 +0100

    Adding custom images

--- config.js
+++ config.js
@@ -1,16 +1,19 @@
-const spreadsheetsId = '1e4GZKOYSND3BRFiW8DJP846s4ecuu8A0wg9PXcVhJ70';
+
+const spreadsheetsId = '19GH7MM8aZVWHyy4Toa_jtDwjzirSHhMgr-10IGums00';
 const spreadsheetsTab = 1;
 const modalFields = [
-        'title', 'description', 'length', 'comments'
+        'title', 'description', 'level', 'length', 'speakers', 'comments'
     ];
 
 const tags = [
-    ['Gestión emocional', 'kids'],
-    ['Deportes y salud', 'sports'],
-    ['Idiomas', 'language'],
-    ['Habilidades y aptitudes', 'skills'],
-    ['Valores o actitudes', 'values'],
-    ['Nuevas tecnologías ', 'technology']
+    ['Herramientas y lenguajes de programación', 'programming'],
+    ['Tendencias tecnológicas', 'future'],
+    ['Robótica, electrónica', 'robotics'],
+    ['Herramientas y técnicas de productividad', 'post-it'],
+    ['Marketing online', 'marketing'],
+    ['Diseño gráfico', 'design'],
+    ['Emprendimiento', 'business'],
+    ['Buen uso de la tecnología', 'cyber-security']
 ];
 
 const colorPalette = ['#DB4367', '#EB4B38', '#AC4988', '#705390', '#3F5279', '#2F4857'];
